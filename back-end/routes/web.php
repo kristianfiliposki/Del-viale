dd4fe6a8ee142ec9f73bf98b13fbaca9d9528795
kristianfiliposki <krisplay@outlook.it>
2024-03-19 17:20:58 +0100

    i correct message controller

--- back-end/routes/web.php
+++ back-end/routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController; //<---- Import del controller precedentemente creato!
-
+use App\Http\Controllers\Admin\MessaggiController; 
+use App\models\Message;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,15 +22,22 @@ Route::get('/',  function () {
 });
 
 
-Route::middleware(['auth'])
-    ->prefix('admin') //definisce il prefisso "admin/" per le rotte di questo gruppo
-    ->name('admin.') //definisce il pattern con cui generare i nomi delle rotte cioè "admin.qualcosa"
-    ->group(function () {
 
-        //Siamo nel gruppo quindi:
-        // - il percorso "/" diventa "admin/"
-        // - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
-        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-    });
+Route::middleware(['auth'])
+->prefix('admin') //definisce il prefisso "admin/" per le rotte di questo gruppo
+->name('admin.') //definisce il pattern con cui generare i nomi delle rotte cioè "admin.qualcosa"
+->group(function () {
+    
+    //Siamo nel gruppo quindi:
+    // - il percorso "/" diventa "admin/"
+    // - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/sms', 
+    /* function () {
+        $sms=Message::all();
+        return view("admin.sms",compact("sms"));
+    } */
+    [MessaggiController::class,'index']);
+});
 
 require __DIR__ . '/auth.php';
